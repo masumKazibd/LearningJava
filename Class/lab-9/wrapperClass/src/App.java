9efd226ff1e5c15cdca3892ee6d502fc5aad1e07
masumkazibd <masum.kazi.hantmk@gmail.com>
2025-05-18 01:28:22 +0800

    learning Wrapper class

---
+++ Class/lab-9/wrapperClass/src/App.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Set;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hellow world");
+        Scanner scanner = new Scanner(System.in);
+        String str;
+        str = scanner.nextLine();
+        String[] lst = str.split("");
+        List<String> ls = new ArrayList<>();
+        Set<String> st = new HashSet<>();
+        for(String item : lst){
+            st.add(str);
+            ls.add(item);
+        }
+        
+        for(String item : lst){
+            System.out.println(item);
+        }
+        System.out.println("List Size:" + ls.size());
+        System.out.println("Set Size:" + st.size());
+
+        Iterator it = st.iterator();
+        while ((it.hasNext())) {{
+            System.out.println(it.next());
+            
+        }
+            
+        }
+    }
+}
