54afff059eebf90763da15c065f79a9643ec5d4e
masumkazibd <masum.kazi.hantmk@gmail.com>
2025-05-18 01:31:24 +0800

    fix

--- Class/lab-9/wrapperClass/src/App.java
+++ Class/lab-9/wrapperClass/src/App.java
@@ -7,13 +7,15 @@ import java.util.Set;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        String str;
         System.out.println("Hellow world");
         Scanner scanner = new Scanner(System.in);
-        String str;
         str = scanner.nextLine();
+
         String[] lst = str.split("");
         List<String> ls = new ArrayList<>();
         Set<String> st = new HashSet<>();
+
         for(String item : lst){
             st.add(str);
             ls.add(item);
@@ -26,11 +28,11 @@ public class App {
         System.out.println("Set Size:" + st.size());
 
         Iterator it = st.iterator();
-        while ((it.hasNext())) {{
-            System.out.println(it.next());
-            
-        }
-            
+        while ((it.hasNext()))
+        {
+            {
+                System.out.println(it.next());
+            }
         }
     }
 }
